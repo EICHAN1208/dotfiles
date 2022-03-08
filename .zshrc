b5babf69f53bb20a044f58255021d13b636dc8fd
murakami <ek.h1.hy@gmail.com>
2022-03-09 00:35:12 +0900

    プラグインにautosuggestion, f-s-highlighting追加

--- .zshrc
+++ .zshrc
@@ -1,5 +1,5 @@
 SCRIPT_DIR=$HOME/dotfiles
 
-source $SCRIPT_DIR/zsh/plugins.zsh
 source $SCRIPT_DIR/zsh/config.zsh
+source $SCRIPT_DIR/zsh/plugins.zsh
 source $SCRIPT_DIR/zsh/alias.zsh
---
+++ zsh/plugins.zsh
@@ -0,0 +1,6 @@
+# https://z-shell.pages.dev/docs/gallery/collection/plugins#sc-autosuggestions-fast-syntax-highlighting
+zi ice wait lucid atinit"ZI[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay"
+zi light z-shell/F-Sy-H
+
+zi ice wait lucid atload"!_zsh_autosuggest_start"
+zi load zsh-users/zsh-autosuggestions
