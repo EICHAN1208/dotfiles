62f16d6909d96da0eb7076f4d98858d97f3c1443
murakami <ek.h1.hy@gmail.com>
2022-04-24 17:44:47 +0900

    add plugin

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -4,3 +4,9 @@ zi light z-shell/F-Sy-H
 
 zi ice wait lucid atload"!_zsh_autosuggest_start"
 zi load zsh-users/zsh-autosuggestions
+
+# ctl + R でコマンド履歴のAND検索
+# https://github.com/z-shell/H-S-MW
+zstyle ":history-search-multi-word" page-size "11"
+zi ice wait lucid
+zi load z-shell/H-S-MW
